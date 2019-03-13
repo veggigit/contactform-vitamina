3ebc64966421af60b00206ae8eff0a75f24d4472
veggigit <estebancajina@gmail.com>
2019-03-13 19:15:25 -0300

    validating, json and save data on db

--- contactform-vitamina.php
+++ contactform-vitamina.php
@@ -15,41 +15,70 @@ License: GPLv2
 */
 
 // load jquey
-add_action( 'wp_enqueue_scripts', 'load_jquery' );
+add_action('wp_enqueue_scripts', 'load_jquery');
 
 function load_jquery()
 {
-    wp_enqueue_script( 'JQUERY_js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '3.3.1', true );
+    wp_enqueue_script('JQUERY_js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '3.3.1', true);
 }
 
 // mi form script
-add_action( 'wp_enqueue_scripts', 'my_form_script' );
+add_action('wp_enqueue_scripts', 'my_form_script');
 
-function my_form_script() 
+function my_form_script()
 {
-    wp_enqueue_script( 'MYJS', plugin_dir_url( __FILE__ ).'js/form.js', array( 'JQUERY_js'), '1.0.0', true );
+    wp_enqueue_script('MYJS', plugin_dir_url(__FILE__) . 'js/form.js', array('JQUERY_js'), '1.0.0', true);
 
     wp_localize_script('MYJS', 'MYJS_var', ['ajaxurl' => admin_url('admin-ajax.php')]);
 }
 
-// function validate
-add_action('wp_ajax_nopriv_launch_validate', 'validate');
-add_action('wp_ajax_launch_validate', 'validate');
+// function validate_send
+add_action('wp_ajax_nopriv_launch', 'validate_send');
+add_action('wp_ajax_launch', 'validate_send');
 
-function validate(){
+function validate_send()
+{
+    // declaramos
+    $get_nombre = $_POST['key_nombre'];
+    $get_email = $_POST['key_email'];
+    $get_body = $_POST['key_body'];
 
-    $get_nombre = ($_POST['nombre']);
-    
-    if (empty($get_nombre) || !isset($get_nombre)) : 
+    // VALIDAMOS 
+    if (!empty($get_nombre) && !empty($get_body) && is_email($get_email)) :
 
-        echo "favor complete los campos";
+        // database
+        global $wpdb;
+        $table = 'contactos';
 
-    else:
-        
-        echo "$get_nombre";
+
+        // saneamos
+        $nombre = sanitize_text_field($get_nombre);
+        $email = sanitize_email($get_email);
+        $body = sanitize_textarea_field($get_body);
+
+        // datos a insertar
+        $data = array(
+            'nombre' => $nombre,
+            'email' => $email,
+            'mensaje' => $body,
+            'fecha' => current_time('mysql')
+        );
+
+        // insertamos
+        $wpdb->insert($table, $data);
+
+        wp_send_json_success('Listo! en breves momentos un ejecutivo se contactrá con UD.', 200);
+
+
+    elseif (!is_email($get_email)) :
+
+        wp_send_json_error('Favor ingrese un email', 400);
+
+    else :
+
+        wp_send_json_error('Favor completel', 400);
 
     endif;
 
-    wp_die();
+    die();
 }
-
--- js/form.js
+++ js/form.js
@@ -1,26 +1,38 @@
 // $(alert('hola'))
-(function($){
-  $(document).on('click','#submit', function(e){
+(function ($) {
+  $(document).on('click', '#submit', function (e) {
     e.preventDefault();
 
     nombre = $('#nombre').val();
+    email = $('#email').val();
+    body = $('#body').val();
 
     $.ajax({
       url: MYJS_var.ajaxurl,
-      type: 'post',
-      data: {
-        nombre: nombre,
-        action: 'launch_validate',
-      },
-
-      success: function(resultado){
-
-        alert(resultado);
-
+      type: 'POST',
+      dataType: 'json',
+      data:
+      {
+        action: 'launch',
+        key_nombre: nombre,
+        key_email: email,
+        key_body: body,
       }
+    })
+
+      .done(function (data, textStatus, jqXHR) {
+
+        alert(jqXHR.responseJSON.data)
+
+      })
+
+      .fail(function (jqXHR, textStatus, errorThrown) {
+
+        alert(jqXHR.responseJSON.data)
+
+      })
 
 
-    });
 
   });
 
