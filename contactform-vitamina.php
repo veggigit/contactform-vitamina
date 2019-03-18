0f5e4579fdb38027eed11f01bd43c9fb3e80f5c2
veggigit <estebancajina@gmail.com>
2019-03-18 12:02:53 -0300

    form validate ok. Add session for avoid multi submit

--- contactform-vitamina.php
+++ contactform-vitamina.php
@@ -14,63 +14,56 @@ License: GPLv2
 * Contact Form Handler
 */
 
-// load jquey
-add_action('wp_enqueue_scripts', 'load_jquery');
-
+// ** LOAD JQUERY ** //
 function load_jquery()
 {
     wp_enqueue_script('JQUERY_js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '3.3.1', true);
 }
+add_action('wp_enqueue_scripts', 'load_jquery');
 
-// mi form script
-add_action('wp_enqueue_scripts', 'my_form_script');
 
-function my_form_script()
+// ** LOAD MY SCRIPT ** //
+function ajax_script()
 {
     wp_enqueue_script('MYJS', plugin_dir_url(__FILE__) . 'js/form.js', array('JQUERY_js'), '1.0.0', true);
-
     wp_localize_script('MYJS', 'MYJS_var', ['ajaxurl' => admin_url('admin-ajax.php')]);
 }
+add_action('wp_enqueue_scripts', 'ajax_script');
 
-// function validate_send
-add_action('wp_ajax_nopriv_launch', 'validate_send');
-add_action('wp_ajax_launch', 'validate_send');
 
-function validate_send()
+// ** FORM SCRIPT ** //
+function use_fomrsript()
 {
-    // declaramos
-    $get_nombre = $_POST['key_nombre'];
-    $get_email = $_POST['key_email'];
-    $get_body = $_POST['key_body'];
+    // sesion
+    session_start();
 
-    // VALIDAMOS 
-    if (!empty($get_nombre) && !empty($get_body) && is_email($get_email)) :
+    // Validate Requests
+    if (!empty($_POST['key_nombre']) && !empty($_POST['key_body']) && is_email($_POST['key_email']) ) :
 
-        // database
-        global $wpdb;
-        $table = 'contactos';
+        if(isset($_SESSION['form_session'])) :
+            
+            wp_send_json_error ($_SESSION['form_session'], 400);
 
+        else:
 
         // saneamos
-        $nombre = sanitize_text_field($get_nombre);
-        $email = sanitize_email($get_email);
-        $body = sanitize_textarea_field($get_body);
+        $nombre = sanitize_text_field($_POST['key_nombre']);
+        $email = sanitize_email($_POST['key_email']);
+        $body = sanitize_textarea_field($_POST['key_body']);
 
-        // datos a insertar
-        $data = array(
-            'nombre' => $nombre,
-            'email' => $email,
-            'mensaje' => $body,
-            'fecha' => current_time('mysql')
-        );
+        $array = array($nombre, $email, $body);
 
-        // insertamos
-        $wpdb->insert($table, $data);
+        // accion after succes validate
+        do_action('after_validate', $array);
 
-        wp_send_json_success('Listo! en breves momentos un ejecutivo se contactrá con UD.', 200);
+        //creamos sesion para impedir doble submit
+        $_SESSION['form_session'] = 'Ya recibimos su mensaje! Si desea enviar otro mensaje favor intente más tarde';
 
+        wp_send_json_success ('ok', 200);
 
-    elseif (!is_email($get_email)) :
+        endif;
+
+    elseif (!is_email($_POST['key_email'])) :
 
         wp_send_json_error('Favor ingrese un email', 400);
 
@@ -82,3 +75,25 @@ function validate_send()
 
     die();
 }
+add_action('wp_ajax_nopriv_launch', 'use_fomrsript');
+add_action('wp_ajax_launch', 'use_fomrsript');
+
+
+// ** AFTER VALIDATE HOOK ** //
+function save_contact($array) 
+{
+    global $wpdb;
+
+    $data = ([
+        'nombre' => $array[0],
+        'email' => $array[1],
+        'mensaje' => $array[2],
+        'fecha' => current_time('mysql')
+    ]);
+
+    $wpdb->insert('contactos', $data);
+
+    wp_mail('estebancajina@gmail.com', 'bla', 'bla');
+
+}
+add_action('after_validate', 'save_contact');
--- js/form.js
+++ js/form.js
@@ -3,9 +3,9 @@
   $(document).on('click', '#submit', function (e) {
     e.preventDefault();
 
-    nombre = $('#nombre').val();
-    email = $('#email').val();
-    body = $('#body').val();
+    nombre = $('#nombre');
+    email = $('#email');
+    body = $('#body');
 
     $.ajax({
       url: MYJS_var.ajaxurl,
@@ -14,15 +14,19 @@
       data:
       {
         action: 'launch',
-        key_nombre: nombre,
-        key_email: email,
-        key_body: body,
+        key_nombre: nombre.val(),
+        key_email: email.val(),
+        key_body: body.val(),
       }
     })
 
       .done(function (data, textStatus, jqXHR) {
 
-        alert(jqXHR.responseJSON.data)
+        alert(jqXHR.responseJSON.data);
+        // clean
+        nombre.val('');
+        email.val('');
+        body.val('');
 
       })
 
